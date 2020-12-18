0bc4bd331c397f51cc5ccbc33c8af55425b0c5c2
Michael Chadwick <michael.chadwick@bet365.com>
2020-12-18 09:24:14 +0000

    tests added for conjuring

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -4,11 +4,12 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
   it("should degrade conjured items twice as fast", function() {
     const gildedRose = new Shop([new Item("Conjured", 10, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(18);
   });
 });
